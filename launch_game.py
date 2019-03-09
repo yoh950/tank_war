9e762e924692aee31f7a8bc5f8aed5111389a5e7
yoh950 <yohann.surville@gmail.com>
2019-03-09 17:25:14 +0100

    shell orientation and movement + object del + begin collide

--- launch_game.py
+++ launch_game.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import math
 
 grey = (197,191,190)
 height = 900
@@ -24,6 +25,7 @@ class challenger:
 		self.position_y = position[1]
 		self.orientation = orientation
 		self.image = pygame.image.load(picture).convert_alpha()
+		self.rect = self.image.get_rect()
 		self.or_to_angle = {
 			"up":0,
 			"left":90,
@@ -34,8 +36,8 @@ class challenger:
 		window.blit(self.image, position)
 
 	def shoot(self):
-		rotate = self.convert_dir_to_angle(self.or_to_angle[self.orientation], self.or_to_angle[new_or])
-		new_shell = shell(self.gun_power, self.or_to_angle[self.orientation], 10, (self.position_x + self.image.get_width() / 2, self.position_y + 250), "assets/images/shell.png")
+		new_shell = shell(self.gun_power, self.or_to_angle[self.orientation], 2,
+			(self.position_x, self.position_y), "assets/images/shell.png", self.image)
 		return new_shell
 
 	def take_damage(self, damage):
@@ -65,28 +67,66 @@ class challenger:
 
 	def convert_dir_to_angle(self, orientation, angle):
     		return 360 - orientation + angle
+
+
 class warrior(challenger):
 	pass
 
 class shell:
-	def __init__(self, damage, direction, speed, position, picture):
+	def __init__(self, damage, direction, speed, position, picture, shooter):
 		self.damage = damage
 		self.direction = direction
 		self.speed = speed
 		self.position_x = position[0]
 		self.position_y = position[1]
 		self.image = pygame.image.load(picture).convert_alpha()
-		window.blit(self.image, position)
+		self.image = pygame.transform.rotate(self.image, direction)
+		self.rect = self.image.get_rect()
+
+		#print(shooter.get_height)
+		if direction == 0:
+			self.position_x = self.position_x + shooter.get_width()/2 
+			self.position_y = self.position_y - self.image.get_height()
+		elif direction == 90:
+			self.position_x = self.position_x - self.image.get_width()
+			self.position_y = self.position_y + shooter.get_height()/2
+		elif direction == 180:
+			self.position_x = self.position_x + shooter.get_width()/2
+			self.position_y = self.position_y + shooter.get_height()
+		elif direction == 270:
+			self.position_x = self.position_x + shooter.get_width()
+			self.position_y = self.position_y + shooter.get_height()/2
+	
+	def move(self):
+		new_pos_x = self.position_x - math.sin(math.radians(self.direction)) * self.speed
+		new_pos_y = self.position_y - math.cos(math.radians(self.direction)) * self.speed
+		if new_pos_y > 0 and new_pos_y < height - self.image.get_height() :
+			self.position_y = new_pos_y
+		else: 
+			return True
+		if new_pos_x > 0 and new_pos_x < width - self.image.get_width() :
+			self.position_x = new_pos_x
+		else:
+			return True
+		return False
+		
 
 
+		
 window.fill(grey)
+
 player = challenger(5, 10, 100, "assets/images/player.png",(width/2, height/2), "left")
+enemy = warrior(5, 10, 100, "assets/images/player.png",(width/3, height/3), "up")
 game_over = False
 pygame.key.set_repeat(150,25)
+game_objects = []
+mun = None
+game_objects.append(player)
+game_objects.append(enemy)
+
 while not game_over:
 	y_move = 0 
 	x_move = 0
-	mun = None
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			game_over= True
@@ -102,11 +142,25 @@ while not game_over:
 				x_move = -1
 			if event.key == pygame.K_SPACE:
 				mun = player.shoot()
+				game_objects.append(mun)
+
 	player.move((x_move, y_move))
 	window.fill(grey)
-	window.blit(player.image, (player.position_x, player.position_y))
-	if mun != None:
-		window.blit(mun.image, (mun.position_x, mun.position_y))
+
+	for game_object in game_objects:
+		rects = [my_object.rect for my_object in game_objects if my_object is not game_object]
+		print(rects)
+		if type(game_object) is shell:
+			if game_object.move() == True:
+				game_objects.remove(game_object)
+		if game_object.rect.collidelist(rects) >= 0:
+			print("youpii")
+		if game_object not in game_objects:
+			del(game_object)
+		else:
+			window.blit(game_object.image, (game_object.position_x, game_object.position_y))
+		
+	
 	pygame.display.flip()
 
 
